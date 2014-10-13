82740a5bdd4467cee057c2b08af957d66eb291af
Pio Baliton Pentin <acheche0910@yahoo.com.ph>
2014-10-13 19:08:30 +0800

    added password for client

--- db_scripts/client.sql
+++ db_scripts/client.sql
@@ -20,3 +20,14 @@ $$
 
 
 language 'sql';
+
+------------------------
+
+create or replace function
+    confirmpassword(in char(5), out char(5))
+returns character as
+$$
+    select password from client
+    where password = $1;
+$$
+    language 'sql';
