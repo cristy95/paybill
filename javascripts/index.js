e85d451af1a223a71a8baeeac0f5ac013cfef89f
raphaeljosebalonga <raphaeljose.balonga@gmail.com>
2014-10-13 23:32:42 +0800

    modified html

--- javascripts/index.js
+++ javascripts/index.js
@@ -17,9 +17,9 @@ function index(accountNum, PLDT_acct_num)
             table += '<thead>' +
                     '<tr>' +
                     '<th>CLIENT NO.</th>' +
-                    '<th>CLIENT ACCOUNT BALANCE</th>' +
-                    '<th>PLDT ACCOUNT NO.</th>'+
-                    '<th>PLDT ACCOUNT BILL</th>' +
+                    '<th>&nbsp;&nbsp;&nbsp;CLIENT ACCOUNT BALANCE</th>' +
+                    '<th>&nbsp;&nbsp;&nbsp;PLDT ACCOUNT NO.</th>'+
+                    '<th>&nbsp;&nbsp;&nbsp;PLDT ACCOUNT BILL</th>' +
                     '</tr>' +
                      '</thead>';
             table += "<tbody>";      
@@ -31,7 +31,7 @@ function index(accountNum, PLDT_acct_num)
               {
 
                 if (j <= 3){
-                  table += '<td>' + row[j] + '</td>';
+                  table += '<td>&nbsp;&nbsp;&nbsp;' + row[j] + '</td>';
                 };
               }
               table += "</tr>";
@@ -45,6 +45,24 @@ function index(accountNum, PLDT_acct_num)
             display = '<div class="table-responsive">No Results Found.<br><br></div>'
             $("#target").html(display);
           }
+      }
+  });
+}
+
+function pay_balance(PLDT_acct_num, bal, acct_num)
+{
+  $.ajax({
+      url: siteloc + scriptloc + "pay_balance.py",
+      data: {PLDT_acct_num:PLDT_acct_num,
+    bal:bal,
+    acct_num:acct_num},
+      dataType: 'json',
+      success: function (res) {
+                  if(res[0][0] != "None")
+                  {
+      str = "Transaction Successful";
+      $("#payresult").html(str);
+                  }
       }
   });
 }
